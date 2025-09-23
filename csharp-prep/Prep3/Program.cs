3c26137a5fe1ce94a92c52378a1db2f016bd7cd4
bdpetersen <bdpetersen@pacbell.net>
2025-09-23 12:51:25 -0600

    completed Prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Guess the Magic Number!");
+        Random rand = new Random();
+        int magicNumber = rand.Next(1, 101);
+        int guess;
+        do
+        {
+            Console.Write("What is your Guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+        } while (guess != magicNumber);
+        Console.WriteLine("That's correct!");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,40 @@ using System;
 
 class Program
 {
+    static void displayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string promptUserName()
+    {
+        Console.Write("Please Enter Your Name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int promptUserNumber()
+    {
+        Console.Write("Please Enter Your Favorite Number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static void promptUserBirthYear(out int birth)
+    {
+        Console.Write("Please Enter The Year You Were Born: ");
+        birth = int.Parse(Console.ReadLine());
+    }
+    static int squareNumber(int i)
+    {
+        return i * i;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcome();
+        string name = promptUserName();
+        int userNumber = promptUserNumber();
+        int year;
+        promptUserBirthYear(out year);
+        int square = squareNumber(userNumber);
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {2025 - year} this year.");
+    
     }
 }
